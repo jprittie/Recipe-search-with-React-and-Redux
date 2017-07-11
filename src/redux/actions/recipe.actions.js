34add443185d1c06fd235df8fde91734c22f1b65
jprittie <eyewallop@gmail.com>
2017-07-10 21:47:26 -0400

    Adds recipe detail action

--- src/redux/actions/recipe.actions.js
+++ src/redux/actions/recipe.actions.js
@@ -1,6 +1,7 @@
 export const RECIPE_ACTIONS = {
   // user actions
   GET_RECIPES_BY_NAME: 'GET_RECIPES_BY_NAME',
+  GET_RECIPE_DETAILS: 'GET_RECIPE_DETAILS',
   INPUT_CHANGE: 'INPUT_CHANGE',
 
   // epic actions
@@ -17,3 +18,8 @@ export const inputChange = value => ({
   type: RECIPE_ACTIONS.INPUT_CHANGE,
   payload: value,
 });
+
+export const getRecipeDetails = recipe => ({
+  type: RECIPE_ACTIONS.GET_RECIPE_DETAILS,
+  payload: recipe
+})
