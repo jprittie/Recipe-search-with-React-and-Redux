c7c60e5ee557e46015c86fede6b5619746b6895a
jprittie <eyewallop@gmail.com>
2017-07-10 21:46:47 -0400

    Imports new components

--- src/App.js
+++ src/App.js
@@ -1,33 +1,37 @@
 import React from 'react';
 import './App.css';
 import { connect } from 'react-redux';
-import { getRecipeByName } from './redux/actions/recipe.actions';
+import { inputChange, getRecipeByName } from './redux/actions/recipe.actions';
 
-import Searchbar from './components/searchbar.js'
+import Searchbar from './components/searchbar';
+import RecipeList from './components/recipelist';
+import RecipeDetail from './components/recipedetail';
 
 
 
 
-const App = ({recipeList, getRecipeByName}) =>
+const App = ({textInput, recipeList, getRecipeByName}) =>
   (
     <div className="App">
       <div className="App-header">
         {recipeList.test}
-        <h2>Welcome to <span className="strike">React</span> Redux-Observable!</h2>
+        <h2>Search Recipes with the Edamam API</h2>
       </div>
       <Searchbar />
-      <p className="App-intro">
-        <button onClick={() => getRecipeByName('omelet')}>Click me for Omelet</button>
-      </p>
-      {recipeList.map(recipe => <h3 key={recipe.uri}>name: {recipe.label} calories: {recipe.calories}</h3>)}
+      <RecipeList />
+      <RecipeDetail />
+
     </div>
   );
+
+
 
 
 //write this out as mapStateToProps and mapDispatchToProps
 const connectConfig = connect(state => ({
   test: 'foo', // how could I potentially apply the value of the reducer on line 6 of reducers/index.js?
   recipeList: state.recipe.list,
+  textInput: state.textInput,
 }), {
   getRecipeByName
 });
