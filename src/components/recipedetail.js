38cd8f60e870c90185ba2371d9660834d464f1e0
jprittie <eyewallop@gmail.com>
2017-07-11 08:55:18 -0400

    Starts adding recipe detail

--- src/components/recipedetail.js
+++ src/components/recipedetail.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 
-const RecipeDetail = () => (
+const RecipeDetail = ({selectedRecipe}) => (
   <div className="recipe-detail">
     <h3>Recipe details</h3>
+    {selectedRecipe.label}
+
   </div>
 
 )
 
-export default RecipeDetail;
+
+const mapStateToProps = (state) => ({
+  selectedRecipe: state.selectedRecipe
+})
+
+
+
+export default connect(mapStateToProps)(RecipeDetail);
