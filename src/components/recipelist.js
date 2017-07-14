4a077899d902c4185a4d43cdc993ebb9045d887c
jprittie <eyewallop@gmail.com>
2017-07-13 22:20:21 -0400

    Adds Links

--- src/components/recipelist.js
+++ src/components/recipelist.js
@@ -1,18 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router';
 import { connect } from 'react-redux';
 import { getRecipeDetails } from '../redux/actions/recipe.actions';
+import RecipeCard from './recipecard';
 
 
-const RecipeList = ({recipeList, recipeCount, isError, onSetSelectedRecipe, selectedRecipe}) => (
+const RecipeList = ({recipeList, isError, onSetSelectedRecipe, selectedRecipe}) => (
 
   <div className="recipe-list">
-  <h3>Search results</h3>
-  <h4>Select a recipe to see details</h4>
-  {recipeList.map(recipe => <button onClick={ () => onSetSelectedRecipe(recipe)} className="recipe-list-button" key={recipe.uri}>{recipe.label}</button>)}
+    <div className="recipe-list-header">
+      <h3>Search results</h3>
+    </div>
+
+    <ul className="card-grid">
+    {recipeList.map(recipe => <li className="card-container-link"><Link to={`/recipe/${recipe.label}`} onClick={ () => onSetSelectedRecipe(recipe)} key={recipe.uri} > <RecipeCard recipe={recipe}/> </Link></li> )}
+    </ul>
   </div>
 
 )
 
+/*    {recipeList.map(recipe => <Link to={`/recipe/${recipe.label}`} onClick={ () => onSetSelectedRecipe(recipe)} className="recipe-list-button" key={recipe.uri}> {recipe.label} </Link> )}*/
 //do error handling here... if state.recipe.list has length 0, show no results found for textInput. However, state.recipe.list could also have length 0 if there is a server error, so that's not a reliable way to handle it
 //if length === 0 && isError: false then there were no search results
 // show server error if isError is true(and change view?)
