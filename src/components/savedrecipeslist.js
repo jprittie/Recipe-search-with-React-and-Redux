138cfef69330798bd5ec1672acecd07b47f123df
jprittie <eyewallop@gmail.com>
2017-07-14 13:09:52 -0400

    Adds conditional message

--- src/components/savedrecipeslist.js
+++ src/components/savedrecipeslist.js
@@ -7,14 +7,19 @@ import RecipeCard from './recipecard';
 
 const SavedRecipesList = ({savedRecipes, onSetSelectedRecipe, selectedRecipe}) => (
 
-  <div className="recipe-list">
-    <div className="recipe-list-header">
-      <h3>Search results</h3>
-    </div>
+  <div>
 
-    <ul className="card-grid">
-    {savedRecipes.map(recipe => <li className="card-container-link"><Link to={`/recipe/${recipe.label}`} onClick={ () => onSetSelectedRecipe(recipe)} key={recipe.uri} > <RecipeCard recipe={recipe}/> </Link></li> )}
-    </ul>
+    { (savedRecipes.length===0) ? <div className="loader"><h1>You don't have any saved recipes at the moment.</h1></div> : <div></div> }
+
+    <div className="recipe-list">
+      <div className="recipe-list-header">
+        <h3>Search results</h3>
+      </div>
+
+      <ul className="card-grid">
+      {savedRecipes.map(recipe => <li className="card-container-link"><Link to={`/recipe/${recipe.label}`} onClick={ () => onSetSelectedRecipe(recipe)} key={recipe.uri} > <RecipeCard recipe={recipe}/> </Link></li> )}
+      </ul>
+    </div>
   </div>
 
 );
