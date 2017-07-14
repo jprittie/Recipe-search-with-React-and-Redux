b720c4c7d87d2b777a26aa11f4ac725bcfdc15bf
jprittie <eyewallop@gmail.com>
2017-07-14 10:14:19 -0400

    Fills out recipe details

--- src/components/recipedetail.js
+++ src/components/recipedetail.js
@@ -1,15 +1,42 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { saveToMyRecipes } from '../redux/actions/saverecipes.actions';
+import { Link } from 'react-router';
 
-//I don't actually need to bring in savedRecipes here
-const RecipeDetail = ({selectedRecipe, onSaveRecipe, savedRecipes}) => (
-  <div className="recipe-detail">
-    <h3>Recipe details</h3>
-    {selectedRecipe.label}
+const RecipeDetail = ({selectedRecipe, onSaveRecipe}) => (
 
-    <div>
-      <button onClick={ () => onSaveRecipe(selectedRecipe)}>Save to My Recipes</button>
+  <div>
+    <div className="recipe-detail-image">
+     <img src={selectedRecipe.image} />
+    </div>
+
+    <div className="recipe-detail">
+      <h3>Recipe details</h3>
+      {selectedRecipe.label}
+
+      <ul className="ingredient-list">
+        {selectedRecipe.ingredientLines.map(ingredient => <li>{ingredient}</li>)}
+      </ul>
+
+      <p>Servings: {selectedRecipe.yield}</p>
+      <p>Calories per serving: {selectedRecipe.calories/selectedRecipe.yield}</p>
+      <Link to={selectedRecipe.url}>Source: {selectedRecipe.source}</Link>
+      <div className="diet-labels">
+        Diet Labels:
+        <ul>
+          {selectedRecipe.dietLabels.map(dietLabel => <li>{dietLabel}</li>)}
+        </ul>
+      </div>
+      <div className="health-labels">
+        Health Labels:
+        <ul>
+          {selectedRecipe.healthLabels.map(healthLabel => <li>{healthLabel}</li>)}
+        </ul>
+      </div>
+
+      <div>
+        <button onClick={ () => onSaveRecipe(selectedRecipe)}>Save to My Recipes</button>
+      </div>
     </div>
   </div>
 
@@ -18,7 +45,6 @@ const RecipeDetail = ({selectedRecipe, onSaveRecipe, savedRecipes}) => (
 
 const mapStateToProps = (state) => ({
   selectedRecipe: state.selectedRecipe,
-  savedRecipes: state.savedRecipes
 })
 
 const actions = {
