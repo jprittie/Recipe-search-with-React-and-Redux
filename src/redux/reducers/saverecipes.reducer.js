1594f16fb4d666b8135befc6af1054d2453af47a
jprittie <eyewallop@gmail.com>
2017-07-14 23:33:10 -0400

    Loads savedRecipes from localstorage

--- src/redux/reducers/saverecipes.reducer.js
+++ src/redux/reducers/saverecipes.reducer.js
@@ -1,8 +1,17 @@
 import { SAVE_RECIPE_ACTIONS } from '../actions/saverecipes.actions'
+import { loadState } from '../../localstorage';
 
-export const savedRecipes = (state = [], action) => {
+const SAVED_RECIPES = loadState();
+
+//check logic
+export const savedRecipes = (state = SAVED_RECIPES || [], action) => {
   if(action.type === SAVE_RECIPE_ACTIONS.SAVE_TO_MY_RECIPES) {
-    return [...state, action.payload]
+    if (SAVED_RECIPES) {
+      return [...state, action.payload]
+    } else {
+      state = [];
+      return [...state, action.payload];
+    }
   }
   return state;
 };
