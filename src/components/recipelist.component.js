858e9bd81cdd17323cc35879fde24940ed75bec4
jprittie <eyewallop@gmail.com>
2017-07-18 12:01:23 -0400

    Updates conditional rendering

--- src/components/recipelist.component.js
+++ src/components/recipelist.component.js
@@ -3,20 +3,19 @@ import { Link } from 'react-router';
 import RecipeCard from './recipecard';
 
 
-const RecipeList = ({recipeList, isError, onSetSelectedRecipe, loadingState, textInput}) => (
+const RecipeList = ({recipeList, isError, onSetSelectedRecipe, loadingState, textInput}) => {
 
-  <div>
+   if (loadingState) {
+       return (<div className="loader"><h2>Loading results...</h2></div>)
 
-    { (loadingState) ? <div className="loader"><h2>Loading results...</h2></div> : <div></div> }
+    } else if (recipeList.length===0 && !isError) {
+       return (<div className="loader"><h2>Sorry, there were no search results.</h2></div>)
 
-    { (recipeList.length===0 && !isError) ? <div className="loader"><h2>Sorry, there were no search results.</h2></div> : <div></div> }
+    } else if (isError) {
+      return (<div className="loader"><h2>Sorry, there was a server error. Please try again later.</h2></div>)
 
-    {/*}
-    { (textInputError) ? <div className="loader"><h2>Please enter a search term.</h2></div> : <div></div> }*/}
-
-    { (isError) ? <div className="loader"><h2>Sorry, there was a server error. Please try again later.</h2></div> :
-
-      <div id="recipe-list">
+    } else if (recipeList.length>0 && !isError && !loadingState) {
+      return (<div id="recipe-list">
         <div id="recipe-list-header" className="twelve columns">
           <h3>search results</h3>
         </div>
@@ -24,12 +23,9 @@ const RecipeList = ({recipeList, isError, onSetSelectedRecipe, loadingState, tex
         <ul id="card-grid" className="twelve columns">
           {recipeList.map(recipe => <li className="card-container-link four columns" key={recipe.uri}><Link to={`/recipe/${recipe.label}`} onClick={ () => onSetSelectedRecipe(recipe)}> <RecipeCard recipe={recipe}/> </Link></li> )}
         </ul>
-      </div>
+      </div>)
     }
 
-  </div>
-
-
-)
+}
 
 export default RecipeList;
--- src/components/savedrecipeslist.component.js
+++ src/components/savedrecipeslist.component.js
@@ -7,17 +7,20 @@ const SavedRecipesList = ({savedRecipes, onSetSelectedRecipe, selectedRecipe}) =
 
   <div>
 
-    { (savedRecipes.length===0) ? <div className="loader"><h2>You don't have any saved recipes at the moment.</h2></div> : <div></div> }
+    { (savedRecipes.length===0) ? <div className="loader"> <h2>You don't have any saved recipes at the moment.</h2> </div> :
 
-    <div id="recipe-list">
-      <div id="recipe-list-header">
-        <h3>my saved recipes</h3>
+      <div id="recipe-list">
+        <div id="recipe-list-header">
+          <h3>my saved recipes</h3>
+        </div>
+
+        <ul id="card-grid">
+        {savedRecipes.map(recipe => <li className="card-container-link four columns"><Link to={`/recipe/${recipe.label}`} onClick={ () => onSetSelectedRecipe(recipe)} key={recipe.uri} > <RecipeCard recipe={recipe}/> </Link></li> )}
+        </ul>
       </div>
 
-      <ul id="card-grid">
-      {savedRecipes.map(recipe => <li className="card-container-link four columns"><Link to={`/recipe/${recipe.label}`} onClick={ () => onSetSelectedRecipe(recipe)} key={recipe.uri} > <RecipeCard recipe={recipe}/> </Link></li> )}
-      </ul>
-    </div>
+    }
+
   </div>
 
 );
