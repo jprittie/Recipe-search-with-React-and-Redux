48eb114482c2a2716aa8dba77be352661670fcd4
jprittie <eyewallop@gmail.com>
2017-07-18 12:01:11 -0400

    Updates searchbar formatting

--- src/components/advancedsearchbar.component.js
+++ src/components/advancedsearchbar.component.js
@@ -6,7 +6,7 @@ const AdvancedSearchbar = ({textInput, searchParameters, onTextInputChange, onCa
 
     <div id="advanced-searchbar-input-group">
 
-        <label htmlFor="advanced-search-input" className="searchbar-input-labels">search by keyword</label>
+        <label htmlFor="advanced-search-input" className="searchbar-input-labels">search by name</label>
         <input
            value={textInput}
            id="advanced-search-input"
@@ -14,32 +14,34 @@ const AdvancedSearchbar = ({textInput, searchParameters, onTextInputChange, onCa
            onChange={ev => onTextInputChange(ev.target.value)}
          />
 
-       <label htmlFor="calories-drop-down" className="searchbar-input-labels">max calories/serving</label>
+       <label htmlFor="calories-drop-down" className="searchbar-input-labels">max. calories</label>
         <select id="calories-drop-down" type="number" onChange={ev => onCaloriesSelect(ev.target.value)}>
           <option selected="selected" value=" "></option>
           <option value="200">200</option>
           <option value="300">300</option>
           <option value="400">400</option>
+          <option value="500">500</option>
+          <option value="600">600</option>
         </select>
 
         <label htmlFor="diet-label-drop-down" className="searchbar-input-labels">diet label</label>
         <select id="diet-label-drop-down" onChange={ev => onDietLabelSelect(ev.target.value)}>
           <option selected="selected" value=" "></option>
-          <option value="high-protein">High Protein</option>
-          <option value="high-fiber">High Fibre</option>
-          <option value="low-fat">Low Fat</option>
-          <option value="low-carb">Low Carb</option>
-          <option value="low-sodium">Low Sodium</option>
+          <option value="high-protein">high protein</option>
+          <option value="high-fiber">high fibre</option>
+          <option value="low-fat">low fat</option>
+          <option value="low-carb">low carb</option>
+          <option value="low-sodium">low sodium</option>
         </select>
 
         <label htmlFor="health-label-drop-down" className="searchbar-input-labels">health label</label>
         <select id="health-label-drop-down" onChange={ev => onHealthLabelSelect(ev.target.value)}>
           <option selected="selected" value=" "></option>
-          <option value="vegetarian">Vegetarian</option>
-          <option value="gluten-free">Gluten Free</option>
-          <option value="dairy-free">Dairy Free</option>
-          <option value="tree-nut-free">Tree Nut Free</option>
-          <option value="low-sugar">Low Sugar</option>
+          <option value="vegetarian">vegetarian</option>
+          <option value="gluten-free">gluten free</option>
+          <option value="dairy-free">dairy free</option>
+          <option value="tree-nut-free">tree nut free</option>
+          <option value="low-sugar">low sugar</option>
         </select>
 
         <Link to={`/search/${searchParameters.textInput}`}>
@@ -53,7 +55,7 @@ const AdvancedSearchbar = ({textInput, searchParameters, onTextInputChange, onCa
              }
            }}
          >
-           search recipes
+           search
          </button>
         </Link>
 
