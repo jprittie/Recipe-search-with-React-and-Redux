ef95dc0db2b95fc390c861f7f5e2a710793b6f63
jprittie <eyewallop@gmail.com>
2017-07-18 09:33:51 -0400

    Updates comments

--- src/index.js
+++ src/index.js
@@ -15,22 +15,16 @@ import { browserHistory } from 'react-router';
 import { saveState } from './localstorage'
 import watch from 'redux-watch'
 
-//do I need to import and install rxjs?
 
 import rootReducer from './redux/reducers';
-import rootEpic from './redux/epics'; //defaults to the index file in the folder
+import rootEpic from './redux/epics';
 
 const loggerMiddleware = createLogger()
 
-//note Purvi used:
-//import configureStore from './store/configure-store';
-//const store = configureStore()
-//she also has a configure-store file
-//her store also has a preloaded state
+
 const store = createStore(
   rootReducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-  // add redux-logger middleware!
   applyMiddleware(createEpicMiddleware(rootEpic), loggerMiddleware),
 );
 
